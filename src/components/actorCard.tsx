fb8b6a221a1f1ce93e90b25af34c7f3f5eea7a24
Priyanshu Rana <priyanshurana1234@gmail.com>
2022-07-14 01:04:20 +0530

    added Header Component and shifted bg-black class to html

--- src/components/actorCard.tsx
+++ src/components/actorCard.tsx
@@ -1,22 +1,22 @@
 import { FC, memo } from "react";
 import { Actor } from "../models/actor";
 
-type actorCardProps = {
+type ActorCardProps = {
   actor: Actor;
 };
 
-const actorCard: FC<actorCardProps> = ({ actor }) => {
+const ActorCard: FC<ActorCardProps> = ({ actor }) => {
   return (
-    <div className="flex flex-col  items-center justify-center sm:px-2 py-3 rounded-lg bg-neutral-800 m-2">
+    <div className="flex flex-col sm:w-32   items-center justify-center sm:px-2 py-3 rounded-lg bg-neutral-800 m-2">
       <img
         src={actor.image.medium}
-        className="rounded-full w-20 h-20 object-cover"
+        className="rounded-full sm:w-20 sm:h-20 object-cover"
       />
       <h1>{actor.name}</h1>
     </div>
   );
 };
 
-actorCard.defaultProps = {};
+ActorCard.defaultProps = {};
 
-export default memo(actorCard);
+export default memo(ActorCard);
